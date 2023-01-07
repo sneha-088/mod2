74de3512a766eb09450ba95b242d22d4a5856619
sneha-088 <117896781+sneha-088@users.noreply.github.com>
2023-01-07 07:29:31 +0000

    my

--- script.js
+++ script.js
@@ -12,22 +12,32 @@ function PrintDeveloperswithMap() {
     if(e.profession === "developer"){
       console.log(e);
     }
-  
-  })
+  });
 }
 
 function PrintDeveloperbyForEach() {
+   array.forEach(element => {
+    if(element.profession === "devloper"){
+      console.log(element);
+    }
+   });
   //Write your code here
 }
 
 function addData() {
-  //Write your code here
+ arr.push({id:4,name:"susan",age:"20",profession:"intern"});
+
 }
 
 function removeAdmin() {
   //Write your code here
+ let RemoveAdmin = arr.filter((arr) => arr.profession!== "admin");
+ console.log(RemoveAdmin);
 }
 
+let NewArray = [{id:5,name:"riya",age:"20",profession:"marketing manager"},{id:6, name:"lucy",age:"25", profession:"bank po"},{id:7, name:"indra", age:"35",profession:"pilot"}]
 function concatenateArray() {
-  //Write your code here
+ let ans = arr.concat(NewArray);
+ console.log(ans);
+ 
 }
